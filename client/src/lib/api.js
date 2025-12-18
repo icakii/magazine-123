b16f00461a608934d1367cfae197d0a59df09433
Icaki <icaki2k@gmail.com>
2025-12-18 18:52:48 +0200

    general fixes 2

--- client/src/lib/api.js
+++ client/src/lib/api.js
@@ -1,24 +1,20 @@
 // client/src/lib/api.js
-import axios from 'axios'
-
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:8080/api'
-
 import axios from "axios"
 
+const API_URL = import.meta.env.VITE_API_URL || "http://localhost:8080/api"
+
 export const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: API_URL,
   withCredentials: true,
 })
 
 // --- FIX ЗА SAFARI / MOBILE ---
 api.interceptors.request.use((config) => {
-  // Проверяваме дали имаме токен в LocalStorage
-  const token = localStorage.getItem('auth_token')
-  
-  // Ако има, го добавяме към хедърите на заявката
+  const token = localStorage.getItem("auth_token")
+
   if (token) {
     config.headers.Authorization = `Bearer ${token}`
   }
-  
+
   return config
 })

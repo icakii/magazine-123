6a16bd85e76d4c1fd32fbf8699734828b0cb3531
Icaki <icaki2k@gmail.com>
2025-12-20 02:48:37 +0200

    cart system 19

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,23 +1,9 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
-import path from "path"
-import { fileURLToPath } from "url"
-
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
 
 export default defineConfig({
   plugins: [react()],
-
   resolve: {
     dedupe: ["react", "react-dom"],
-    alias: {
-      react: path.resolve(__dirname, "node_modules/react"),
-      "react-dom": path.resolve(__dirname, "node_modules/react-dom"),
-    },
-  },
-
-  optimizeDeps: {
-    include: ["react", "react-dom", "react-router-dom"],
   },
 })

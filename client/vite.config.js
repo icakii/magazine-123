e481a27ea71b13076c296612e706375ff4a07313
Icaki <icaki2k@gmail.com>
2025-12-20 16:42:16 +0200

    web service fix 7

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,8 +2,8 @@ import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 
 export default defineConfig({
+  base: "/", // <-- важно
   plugins: [react()],
-  resolve: {
-    dedupe: ["react", "react-dom"],
-  },
+  resolve: { dedupe: ["react", "react-dom"] },
+  optimizeDeps: { include: ["react", "react-dom"] },
 })

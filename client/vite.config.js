b569fe912d5b40533868eaa3a247dcf4c5c76ade
Icaki <icaki2k@gmail.com>
2025-12-20 17:48:23 +0200

    web service fix 12

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,14 +4,14 @@ import path from "path"
 
 export default defineConfig({
   plugins: [react()],
+
   resolve: {
+    dedupe: ["react", "react-dom"],
     alias: {
       react: path.resolve(__dirname, "node_modules/react"),
       "react-dom": path.resolve(__dirname, "node_modules/react-dom"),
+      "react/jsx-runtime": path.resolve(__dirname, "node_modules/react/jsx-runtime"),
+      "react/jsx-dev-runtime": path.resolve(__dirname, "node_modules/react/jsx-dev-runtime"),
     },
-    dedupe: ["react", "react-dom"],
-  },
-  optimizeDeps: {
-    include: ["react", "react-dom", "react-router-dom"],
   },
 })
